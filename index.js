b353e771758f9ed8c014156002e3509504e6a367
unknown <achahboune@gmail.com>
2026-03-17 00:07:08 +0000

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("TrendDash is LIVE 🚀");
+});
+
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
